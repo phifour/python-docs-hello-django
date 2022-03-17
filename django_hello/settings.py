61c645814667e9b3b79ad481961c2b0806a0190c
phifour <w.frisch@gmx.at>
2022-03-17 20:49:34 +0100

    Update settings.py

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -74,14 +74,30 @@ WSGI_APPLICATION = 'django_hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
-DATABASES = {    
+#"""
+#DATABASES = {    
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }    
+#}
+#"""
+
+
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }    
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
-"""
+
+https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
 
 
 # Password validation
